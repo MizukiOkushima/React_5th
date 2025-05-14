331580a29a9696548c64e8efac47982e6ed29b02
MizukiOkushima <caresupplement@icloud.com>
2025-05-14 21:39:40 +0900

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import Button from "./components/Button/Button";
 
 function App() {
-  const handleClick = () => {
-    console.log("ボタンがクリックされました");
-  };
+  // const handleClick = () => {
+  //   console.log("ボタンがクリックされました");
+  // };
   return (
     <>
       <h1>Hello World</h1>
-      <Button type="button" disabled={false} onClick={handleClick}>
+      {/* <Button type="button" disabled={false} onClick={handleClick}> */}
+      {/* <Button type="button" disabled={false} >
         <span>ボタンクリック</span>
-      </Button>
+      </Button> */}
+      <Button type="button" disabled={false} />
     </>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,18 +1,30 @@
 import styles from "./Button.module.css";
+import { useState } from "react";
 
 function Button(props) {
-  const { type, disabled, children, onClick } = props;
+
+  const [count, setCount] = useState(0);
+  // const { type, disabled, children } = props;
+  const { type, disabled } = props;
+
   //   const handleClick = () => {
   //     alert("ボタンがクリックされました");
   //   };
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
 
   return (
     // <button type={type} onClick={handleClick}>
     //   <i>icon</i>
     //   <span>クリック</span>
     // </button>
-    <button className={styles.button} type={type} disabled={disabled} onClick={onClick}>
-      {children}
+    // <button className={styles.button} type={type} disabled={disabled} onClick={handleClick}>
+    //   {children}
+    // </button>
+    <button className={styles.button} type={type} disabled={disabled} onClick={handleClick}>
+      カウント：{count}
     </button>
   );
 }
